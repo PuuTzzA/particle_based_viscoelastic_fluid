441636dcfb45855b3db5462a775601d5e3c23563
PuuTzzA <putzerthomas03@gmail.com>
2025-03-13 23:09:50 +0100

    copy template

---
+++ src/script/script_inputs.js
@@ -0,0 +1,26 @@
+document.addEventListener("mousemove", (e) => {
+})
+
+document.getElementById("slider_1").addEventListener("input", e => {
+    document.getElementById("slider_1_out").innerHTML = e.target.value;
+    // console.log(e.target.value);
+})
+
+document.getElementById("slider_2").addEventListener("input", e => {
+    document.getElementById("slider_2_out").innerHTML = e.target.value;
+    // console.log(e.target.value);
+})
+
+document.getElementById("slider_3").addEventListener("input", e => {
+    document.getElementById("slider_3_out").innerHTML = e.target.value;
+    // console.log(e.target.value);
+})
+
+document.getElementById("slider_4").addEventListener("input", e => {
+    document.getElementById("slider_4_out").innerHTML = e.target.value;
+    // console.log(e.target.value);
+})
+
+document.getElementById("button_1").addEventListener("click", e => {
+    // console.log("button_1");
+})
